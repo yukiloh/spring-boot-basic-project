8359c8f2495f7b2b7d7de41fd3d680826a9a85b1
Ash <yukiloh001@gmail.com>
2019-11-19 23:21:10 +0800

    update;

--- SpringBoot_Project_05_AdminLET/src/main/java/com/example/AdminLET/controller/MainController.java
+++ SpringBoot_Project_05_AdminLET/src/main/java/com/example/AdminLET/controller/MainController.java
@@ -1,23 +1,49 @@
 package com.example.AdminLET.controller;
 
+import com.example.AdminLET.domain.Message;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
 
-    @GetMapping({"/","login"})
-    public String login(){
+    @GetMapping({"/", "login"})
+    public String login() {
         return "login";
     }
 
-    @PostMapping({"/index"})
-    public String index(
+    @PostMapping({"/login.do"})
+    public String login(
             @RequestBody(required = false) String username,
-            @RequestBody(required = false) String password){
-        System.out.println("username:"+username);
-        System.out.println("password:"+password);
+            @RequestBody(required = false) String password) {
+        System.out.println("username:" + username);
+        System.out.println("password:" + password);
+        return "redirect:/index";
+    }
+
+    @GetMapping({"/index"})
+    public String index() {
         return "index";
-    }}
+    }
+
+
+
+
+
+    @ResponseBody
+    @PostMapping({"/{uuid}/msg"})
+    public Message recentMessage(@PathVariable String uuid){
+//        System.out.println(uuid);   /*模拟获取uuid*/
+        Message message = new Message();
+        message.setMessage("hello");/*模拟获取json数据*/
+        return message;
+    }
+
+
+
+    /* for test! */
+    @GetMapping({"/500"})
+    public String fiveZeroZero() {
+        return "/error/500";
+    }
+}
